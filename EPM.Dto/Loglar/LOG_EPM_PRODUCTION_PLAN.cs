2221aea70eee26404651a642c3226c30e5b11e77
erdalklc <eerdal.klc@hotmail.com>
2021-12-03 17:21:14 +0300

    erdal

---
+++ EPM.Dto/Loglar/LOG_EPM_PRODUCTION_PLAN.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPM.Dto.Loglar
+{
+    public class LOG_EPM_PRODUCTION_PLAN : LOG
+    {
+        public int ID { get; set; }
+    }
+}
